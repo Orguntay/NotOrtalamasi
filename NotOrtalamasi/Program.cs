a87ed7df68b507ea8226df4a7acbba5b584a36f8
Orguntay <67823328+Orguntay@users.noreply.github.com>
2020-08-14 11:43:09 +0300

    ilk calisan versiyon

---
+++ NotOrtalamasi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NotOrtalamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> notlar = new List<double>();
+            int i = 0;
+            string x;
+            string ciksinMi = "";
+            do
+            {
+                Console.WriteLine("Lütfen {0}. notu giriniz :", i + 1);
+                x = Console.ReadLine();
+                notlar.Add(Convert.ToDouble(x));
+                Console.WriteLine("Hesaplansın mı? E/H : ");
+                ciksinMi = Console.ReadLine();
+                i++;
+            }
+            while (!(ciksinMi == "E" || ciksinMi == "e"));
+            Console.WriteLine("Notlar toplamı : {0}", notlar.Sum());
+            double ortalama = notlar.Sum() / (i);
+            Console.WriteLine("Not ortalaması : {0}", ortalama);
+        }
+    }
+}
